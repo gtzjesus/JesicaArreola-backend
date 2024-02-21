ff4af7c8f19bc8c2b9081c69fe912b66390d4260
gtz.jesus <gtz.jesus@outlook.com>
2024-02-21 16:57:07 -0700

    Hosting backend to heroku to start making frontend requests from database

--- src/routes/dataRoutes.js
+++ src/routes/dataRoutes.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getHouses } = require('../controllers/dataController');
+
+router.get('/houses', getHouses);
+
+module.exports = router;
