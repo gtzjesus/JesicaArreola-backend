bbcd73bb22377488c0ca4c75df4ac4ff7a4fa2f7
gtz.jesus <gtz.jesus@outlook.com>
2024-02-21 22:08:12 -0700

    Modding package.json

--- src/server.mjs
+++ src/server.mjs
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
   res.send('WorldHello!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    'Express server listening on port %d in %s mode',
+    this.address().port,
+    app.settings.env
+  );
 });
