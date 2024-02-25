e938259e10fe5661590e03ee72b03312e4ddd7ac
gtz.jesus <gtz.jesus@outlook.com>
2024-02-25 12:16:18 -0700

    ES6 syntax package support

--- src/routes/supabaseRoute.mjs
+++ src/routes/supabaseRoute.mjs
@@ -1,15 +1,19 @@
 import express from 'express';
 import { getHouses } from '../controllers/dataController.mjs'; // Import your Supabase controller function
 
+// Create a router to define routes for API
 const router = express.Router();
 
-// Define route to fetch houses
+// Define a GET route to aquire the houses (callback function gets excecuted)
 router.get('/api/houses', async (req, res) => {
   try {
+    // Call controller module to fetch houses data
     const houses = await getHouses();
     res.json(houses);
   } catch (error) {
     console.error('Error fetching houses:', error);
+
+    // Send error back to client alogn with a JSON object containing the error message
     res.status(500).json({ error: 'Failed to fetch houses from Supabase' });
   }
 });
