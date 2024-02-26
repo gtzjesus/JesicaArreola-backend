d0026fe15c966377854afdcf0181cbfacf395c72
gtz.jesus <gtz.jesus@outlook.com>
2024-02-26 12:07:38 -0700

    Supabase managed

--- src/services/supabase.mjs
+++ src/services/supabase.mjs
@@ -1,5 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-
 const supabaseUrl = 'https://dpzeaijawvrbqlrgllyv.supabase.co';
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwemVhaWphd3ZyYnFscmdsbHl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg1NTYyNzEsImV4cCI6MjAyNDEzMjI3MX0.pZGFClEsWRhqlSxCT--uK6FiNNnyy_0snpujRc_3nyM';
